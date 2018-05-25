1b27ddd4647858ed5d84cdbcd4dda8a5b24645ad
HollowVin <corraljuanmiguel@gmail.com>
2018-05-25 15:25:43 -0500

    Add project files.

---
+++ Monitor/Monitor.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="VB" AutoEventWireup="false" CodeFile="Monitor.aspx.vb" Inherits="Monitor" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            IP:
+            <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+            <br />
+            Port:
+            <asp:Label ID="Label2" runat="server" Text="Label"></asp:Label>
+            <br />
+            <br />
+            <div>
+            </div>
+            <br />
+            Temperature:
+            <asp:Label ID="Label3" runat="server" Text="Label"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Monitor/Monitor.aspx.vb
@@ -0,0 +1,38 @@
+﻿
+Imports System.Net
+Imports System.Net.Sockets
+
+Partial Class Monitor
+    Inherits System.Web.UI.Page
+
+    Private Listener As TcpListener
+
+    Private Sub Monitor_Load(sender As Object, e As EventArgs) Handles Me.Load
+        EstablishListener()
+
+        While True
+            HandleConnections()
+        End While
+    End Sub
+
+    Private Sub Monitor_Disposed(sender As Object, e As EventArgs) Handles Me.Disposed
+        Try
+            Listener.Stop()
+        Catch SException As SocketException
+        End Try
+    End Sub
+
+    Private Sub EstablishListener()
+        Dim ipHostInfo As IPHostEntry = Dns.GetHostEntry(Dns.GetHostName())
+        Dim ipAddress As IPAddress = ipHostInfo.AddressList(0)
+        Dim localEndPoint As New IPEndPoint(ipAddress, 11000)
+
+        Listener = New TcpListener(ipAddress, 100)
+        Listener.Start()
+    End Sub
+
+    Private Sub HandleConnections()
+
+    End Sub
+
+End Class
