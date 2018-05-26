32b9c89c2f28103eacb994ba238a7957e878a8ba
HollowVin <corraljuanmiguel@gmail.com>
2018-05-26 14:55:32 -0500

    Added AJAX timer and styles

--- Monitor/Monitor.aspx
+++ Monitor/Monitor.aspx
@@ -15,8 +15,12 @@
             width: 197px;
         }
     </style>
+    <link href="StyleSheet.css" rel="stylesheet" type="text/css" />
 </head>
 <body>
+    <div class="newStyle1" style="height: 101px">
+                Monitor de sensores</div>
+    <div class="content">
     <form id="form1" runat="server">
             <asp:ScriptManager ID="ScriptManager1" runat="server">
             </asp:ScriptManager>
@@ -33,15 +37,27 @@
                     Connected Sensors:
                     <asp:Label ID="LabelNSensors" runat="server"></asp:Label>
                     <br />
-                    <asp:ListBox ID="ListBoxSensors" runat="server" Height="146px" Width="244px"></asp:ListBox>
+                    Status:
+                    <asp:Label ID="LabelStatus" runat="server"></asp:Label>
                     <br />
-                    <asp:TextBox ID="TextLog" runat="server" Height="136px" Width="232px" AutoPostBack="True"></asp:TextBox>
+                    <br />
+                    <asp:Table ID="Table1" runat="server" GridLines="Both" Width="329px">
+                        <asp:TableRow runat="server" Font-Bold="True">
+                            <asp:TableCell runat="server">Sensor</asp:TableCell>
+                            <asp:TableCell runat="server">Value</asp:TableCell>
+                        </asp:TableRow>
+                        <asp:TableRow runat="server">
+                            <asp:TableCell runat="server">Temperature</asp:TableCell>
+                            <asp:TableCell runat="server">(N/A)</asp:TableCell>
+                        </asp:TableRow>
+                    </asp:Table>
+                    <br />
+                    <asp:Timer ID="Timer1" runat="server" Interval="1000">
+                    </asp:Timer>
                 </ContentTemplate>
-                <Triggers>
-                    <asp:AsyncPostBackTrigger ControlID="TextLog" EventName="TextChanged" />
-                </Triggers>
             </asp:UpdatePanel>
             <br />
     </form>
+    </div>
 </body>
 </html>
--- Monitor/Monitor.aspx.vb
+++ Monitor/Monitor.aspx.vb
@@ -6,26 +6,17 @@ Imports Client
 Partial Class Monitor
     Inherits System.Web.UI.Page
 
-    Private Listener As TcpListener
-    Private ConnectedSensors As New Hashtable()
+    Private Shared Listener As TcpListener
+    Private Shared ConnectedSensors As Hashtable
+    Private Shared Status As String
+    Private Shared Data As String
 
     Private Sub Monitor_Load(sender As Object, e As EventArgs) Handles Me.Load
-        Dim ListenThread = New Threading.Thread(AddressOf DoListen)
-        ListenThread.Start()
-    End Sub
-
-    Private Sub Monitor_Disposed(sender As Object, e As EventArgs) Handles Me.Disposed
-        Try
-            Listener.Stop()
-        Catch
-        End Try
-    End Sub
-
-    Private Sub Monitor_Unload(sender As Object, e As EventArgs) Handles Me.Disposed
-        Try
-            Listener.Stop()
-        Catch
-        End Try
+        If Not Page.IsPostBack Then
+            ConnectedSensors = New Hashtable()
+            Dim ListenThread = New Threading.Thread(AddressOf DoListen)
+            ListenThread.Start()
+        End If
     End Sub
 
     Private Sub DoListen()
@@ -39,12 +30,13 @@ Partial Class Monitor
                 AddHandler x.ValueReceived, AddressOf OnLineReceived
 
                 ConnectedSensors.Add(x.ID, x)
+                UpdateStatus("Temperature sensor connected")
 
                 LabelNSensors.Text = ConnectedSensors.Values.Count
 
             Loop Until False
         Catch E As Exception
-            Console.WriteLine(E.ToString())
+            System.Diagnostics.Debug.WriteLine(E.ToString())
         End Try
     End Sub
 
@@ -71,8 +63,9 @@ Partial Class Monitor
         ConnectedSensors.Remove(sender.ID)
     End Sub
 
-    Private Sub OnLineReceived(ByVal sender As Client, ByVal Data As String)
-        UpdateStatus("Line:" & Data)
+    Private Sub OnLineReceived(ByVal sender As Client, ByVal ReceivedData As String)
+        UpdateStatus("Line:" & ReceivedData)
+        Data = ReceivedData
 
         'Dim objClient As Client
         'Dim d As DictionaryEntry
@@ -83,7 +76,13 @@ Partial Class Monitor
         'Next
     End Sub
 
-    Private Sub UpdateStatus(ByVal Status As String)
-        TextLog.Text = Status
+    Private Sub UpdateStatus(ByVal Stat As String)
+        Status = Stat
+    End Sub
+
+    Protected Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        LabelNSensors.Text = ConnectedSensors.Values.Count
+        LabelStatus.Text = Status
+        Table1.Rows(1).Cells(1).Text = Data
     End Sub
 End Class
